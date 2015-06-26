b25e8074fca1bbb9aca0acb3ee64b83550af9e83
slofuno <slofurno@gmail.com>
2015-06-26 18:10:40 -0400

    t

--- httplistener/Program.cs
+++ httplistener/Program.cs
@@ -47,7 +47,7 @@ namespace httplistener
       {
        // var context = await listener.GetContextAsync().ConfigureAwait(false);
         TcpClient client = await server.AcceptTcpClientAsync();
-        Serve(client);
+        Task.Run(()=>Serve(client));
 
       }
     }
