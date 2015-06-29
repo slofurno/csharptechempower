0795c24f7910481a17b0719ccbd3877be51bd1d6
slofuno <slofurno@gmail.com>
2015-06-29 04:22:40 -0400

    queue

--- httplistener/Program.cs
+++ httplistener/Program.cs
@@ -82,8 +82,10 @@ namespace httplistener
 
       var endpoint = new IPEndPoint(IPAddress.Any, 8080);
       listenSocket = new Socket(endpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-      listenSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
+      listenSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, false);
       listenSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.DontLinger, true);
+      listenSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.NoDelay, true);
+      
 
       listenSocket.Bind(endpoint);
       listenSocket.Listen(4000);
